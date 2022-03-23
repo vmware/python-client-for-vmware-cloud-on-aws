5264cc4de196fa51d2ad01e7f22eaf287fb43aa3
maulepilot117 <whitech@vmware.com>
2022-03-23 18:09:59 -0400

    Renamed refactored functions
    Signed-off-by: maulepilot117 <whitech@vmware.com>

--- pyvmc_csp.py
+++ pyvmc_csp.py
@@ -1,28 +1,32 @@
 import json
 import requests
 
-def getSddcs (strProdURL, orgID, sessiontoken):
+def get_sddcs_json(strProdURL, orgID, sessiontoken):
+    """Returns list of all SDDCs in an Org via json"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f"{strProdURL}/vmc/api/orgs/{orgID}/sddcs"
     response = requests.get(myURL, headers=myHeader)
     jsonResponse = response.json()
     return jsonResponse
 
-def getSDDCInfo (strProdURL, orgID, sessiontoken, sddcID):
+def get_sddc_info_json (strProdURL, orgID, sessiontoken, sddcID):
+    """Returns SDDC info in JSON format"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f"{strProdURL}/vmc/api/orgs/{orgID}/sddcs/{sddcID}"
     response = requests.get(myURL, headers=myHeader)
     jsonresponse = response.json()
     return jsonresponse
 
-def getConnectedAccounts(strProdURL, orgID, sessiontoken):
+def get_connected_accounts_json(strProdURL, orgID, sessiontoken):
+    """ Returns all connected AWS accounts in json format """
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f"{strProdURL}/vmc/api/orgs/{orgID}/account-link/connected-accounts"
     response = requests.get(myURL, headers=myHeader)
     jsonResponse = response.json()
     return jsonResponse
 
-def getCompatSubnets (strProdURL, orgID, sessiontoken, linkedAWSID, region):
+def get_compatible_subnets_json(strProdURL, orgID, sessiontoken, linkedAWSID, region):
+    """Returns all compatible subnets for linking in selected AWS Account and AWS Region"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f"{strProdURL}/vmc/api/orgs/{orgID}/account-link/compatible-subnets"
     params = {'org': orgID, 'linkedAccountId': linkedAWSID,'region': region}
@@ -30,7 +34,8 @@ def getCompatSubnets (strProdURL, orgID, sessiontoken, linkedAWSID, region):
     jsonResponse = response.json()
     return jsonResponse
 
-def getCSPUsers (strCSPProdURL, orgID, sessiontoken):
+def get_csp_users_json(strCSPProdURL, orgID, sessiontoken):
+    """Returns all CSP Users in the select ORG in JSON format"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{strCSPProdURL}/csp/gateway/am/api/v2/orgs/{orgID}/users'
     response = requests.get(myURL, headers=myHeader)
--- pyvmc_nsx.py
+++ pyvmc_nsx.py
@@ -1,21 +1,27 @@
 import json
 import requests
 
-def vpnPublicIP (proxy_url, sessiontoken):
+
+def vpn_public_ip_json(proxy_url, sessiontoken):
+    """Returns JSON response with SDDC User Config"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/cloud-service/api/v1/infra/sddc-user-config'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
 
-def getCGWSegments (proxy_url, sessiontoken):
+
+def get_cgw_segments_json(proxy_url, sessiontoken):
+    """Returns JSON response with all CGW segments in the SDDC"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-1s/cgw/segments'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
 
-def getSDDCNATInfo (proxy_url, sessiontoken):
+
+def get_sddc_nat_info_json(proxy_url, sessiontoken):
+    """Returns JSON response with SDDC NAT rules"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-1s/cgw/nat/USER/nat-rules'
     response = requests.get(myURL, headers=myHeader)
@@ -23,7 +29,9 @@ def getSDDCNATInfo (proxy_url, sessiontoken):
     json_response_status_code = response.status_code
     return json_response, json_response_status_code
 
-def getNATStats (proxy_url, sessiontoken, nat_id):
+
+def get_nat_stats_json(proxy_url, sessiontoken, nat_id):
+    """Returns JSON response with NAT statistics for selected NAT rule"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-1s/cgw/nat/USER/nat-rules/{nat_id}/statistics'
     response = requests.get(myURL, headers=myHeader)
@@ -31,7 +39,9 @@ def getNATStats (proxy_url, sessiontoken, nat_id):
     json_response_status_code = response.status_code
     return json_response, json_response_status_code
 
-def getSDDCVpnInfo (proxy_url, sessiontoken):
+
+def get_sddc_vpn_info_json(proxy_url, sessiontoken):
+    """Returns JSON response with VPN session info"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-0s/vmc/locale-services/default/ipsec-vpn-services/default/sessions'
     response = requests.get(myURL, headers=myHeader)
@@ -39,31 +49,38 @@ def getSDDCVpnInfo (proxy_url, sessiontoken):
     json_response_status_code = response.status_code
     return json_response, json_response_status_code
 
-def getVPNIPSecProfile (proxy_url, sessiontoken):
+
+def get_vpn_ipsec_profile_json(proxy_url, sessiontoken):
+    """Returns JSON response with VPN IPSEC profiles for the SDDC"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/ipsec-vpn-tunnel-profiles'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
 
-def getVPNIKEProfile (proxy_url, sessiontoken):
+
+def get_vpn_ike_profile_json(proxy_url, sessiontoken):
+    """Returns JSON response with VPN IKE profiles for the SDDC"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/ipsec-vpn-ike-profiles'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
 
-def getL2VPNService (proxy_url, sessiontoken):
+
+def get_l2vpn_service_json(proxy_url, sessiontoken):
+    """Returns JSON response with L2VPN services"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-0s/vmc/locale-services/default/l2vpn-services/default'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
 
-def getL2VPNSession (proxy_url, sessiontoken):
+
+def get_l2vpn_session_json(proxy_url, sessiontoken):
+    """Returns JSON response with L2VPN session"""
     myHeader = {'csp-auth-token': sessiontoken}
     myURL = f'{proxy_url}/policy/api/v1/infra/tier-0s/vmc/locale-services/default/l2vpn-services/default/sessions'
     response = requests.get(myURL, headers=myHeader)
     json_response = response.json()
     return json_response
-
